f1a809cac34a775e07ebfbd4ee1f622284a113d5
iuliancarnaru <iulian.carnaru@yahoo.co.uk>
2019-01-23 16:35:37 +0000

    learning about CONTEXT and implementing in App

--- src/App.js
+++ src/App.js
@@ -2,22 +2,90 @@ import React from "react";
 import { render } from "react-dom";
 import Results from "./Results";
 import { Router, Link } from "@reach/router";
+import pf from "petfinder-client";
+import { Provider } from "./SearchContext";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
+require("dotenv").config();
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      location: "Seatle, WA",
+      animal: "",
+      breed: "",
+      breeds: [],
+      handleAnimalChange: this.handleAnimalChange,
+      handleBreedChange: this.handleBreedChange,
+      handleLocationChange: this.handleLocationChange,
+      getBreeds: this.getBreeds
+    };
+  }
+
+  handleLocationChange = event => {
+    this.setState({
+      location: event.target.value
+    });
+  };
+
+  handleAnimalChange = event => {
+    this.setState(
+      {
+        animal: event.target.value
+      },
+      this.getBreeds
+    );
+  };
+  handleBreedChange = event => {
+    this.setState({
+      breed: event.target.value
+    });
+  };
+  getBreeds() {
+    if (this.state.animal) {
+      petfinder.breed
+        .list({ animal: this.state.animal })
+        .then(data => {
+          if (
+            data.petfinder &&
+            data.petfinder.breeds &&
+            Array.isArray(data.petfinder.breeds.breed)
+          ) {
+            this.setState({
+              breeds: data.petfinder.breeds.breed
+            });
+          } else {
+            this.setState({ breeds: [] });
+          }
+        }) /* eslint-disable-next-line */
+        .catch(console.error);
+    } else {
+      this.setState({
+        breeds: []
+      });
+    }
+  }
+
   render() {
     return (
       <div>
         <header>
           <Link to="/">Adopt Me</Link>
         </header>
-
-        <Router>
-          <Results path="/" />
-          <Details path="/details/:id" />
-          <SearchParams path="/search-params" />
-        </Router>
+        <Provider value={this.state}>
+          <Router>
+            <Results path="/" />
+            <Details path="/details/:id" />
+            <SearchParams path="/search-params" />
+          </Router>
+        </Provider>
       </div>
     );
   }
---
+++ src/SearchContext.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const SearchContext = React.createContext({
+  location: "Seatle, WA",
+  animal: "",
+  breed: "",
+  breeds: [],
+
+  handleAnimalChange() {},
+
+  handleBreedChange() {},
+
+  handleLocationChange() {},
+
+  getBreeds() {}
+});
+
+export const Provider = SearchContext.Provider;
+export const Consumer = SearchContext.Consumer;
